fb437d96fdbc0945cb48b66719d27b8ad6fe07f6
Shridhar Naik <shridharnaik05802@gmail.com>
2026-05-18 20:00:23 +0530

    Proved reverse_append theorem

--- Main.lean
+++ Main.lean
@@ -10,3 +10,10 @@ theorem reverse_singleton (x : Nat) : myReverse [x] = [x] := by rfl
 
 theorem reverse_append_nil (xs : List Nat) : myReverse (xs ++ []) = myReverse xs := by
  rw [List.append_nil]
+
+theorem reverse_append (xs ys : List Nat) : myReverse (xs ++ ys) = myReverse ys ++ myReverse xs := by
+    induction xs with
+    | nil =>
+       rw [List.nil_append, myReverse, List.append_nil]
+    | cons x xs ih =>
+       rw [List.cons_append, myReverse, ih, List.append_assoc, myReverse]
