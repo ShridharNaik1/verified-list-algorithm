4bbc2d963c0b043516828f0614f2c5702bc95de8
Shridhar Naik <shridharnaik05802@gmail.com>
2026-05-23 15:09:51 +0530

    Proved reverse_length

--- Main.lean
+++ Main.lean
@@ -28,3 +28,11 @@ theorem reverse_reverse (xs : List Nat) : myReverse (myReverse xs) = xs := by
 
 theorem reverse_cons (x : Nat) (xs : List Nat) : myReverse (x :: xs) = myReverse xs ++ [x] := by
     rfl
+
+theorem reverse_length (xs : List Nat) : (myReverse xs).length = xs.length := by
+    induction xs with
+    | nil =>
+       rfl
+    | cons x xs ih =>
+       rw [myReverse, List.length_append, List.length_singleton, ih]
+       rfl
